be2e5939a8d146751cb5f5b1dc9fc9b8c39302dc
Guillaume Algis <guillaume.algis@gmail.com>
2021-05-15 23:57:18 +0200

    Initial commit

---
+++ ProductNameXcconfig/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ProductNameXcconfig
+//
+//  Created by Guillaume Algis on 15/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        
+    }
+
+
+}
+
